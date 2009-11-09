d47e207d1835be546846825c7dab740b3a83bfc5
Daniel Ralston <Wubbulous@gmail.com>
2009-11-08 21:11:40 -0800

    Added main.h

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,4 +1,13 @@
+#include "main.h"
+extern void* end;
+
 int kmain(struct multiboot *mboot)
 {
   return 0xDEADBEEF;
 }
+
+struct mmap *newmmap(void *begin)
+{
+  
+}
+ 
---
+++ src/kernel/main.h
@@ -0,0 +1,10 @@
+struct mmap
+{
+  int gran;
+  int size;
+  long *map;
+  int oldest_free_index;
+  int newest_free_index;
+  int free[1024];
+}
+
